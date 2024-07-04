6cf8540b9bba23e8e065c804d9f5f61ba13b033e
IngaKon <kononova.inga@list.ru>
2024-07-04 15:42:44 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = ('Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron')
+aaron=(5+3+3+5+4)/5
+Bilbo=(2+2+2+3)/4
+Johnny=(4+5+5+2)/4
+Khendrik=(4+4+3)/3
+Steve=(5+5+5+4+5)/5
+average_score={'Aaron': aaron,'Bilbo':Bilbo,'Johnny':Johnny,'Khendrik':Khendrik,'Steve':Steve}
+print(average_score)
+print(average_score["Aaron"])
